f9d36eba34693923e19b55eb8e6161ad588e0b26
YuliaConstien <91601072+YuliaConstien@users.noreply.github.com>
2023-07-18 23:45:59 +0200

    Add files via upload

---
+++ src/NaiveBayes.py
@@ -0,0 +1,80 @@
+import os
+import json
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.naive_bayes import  MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+# change paths
+#trainJSON = add path to dataset
+#testJSON = add path to dataset
+
+def getData(inJSON):
+
+    with open(inJSON, 'r') as f:
+      data = json.load(f)
+
+    TrainTexts = []
+    TrainLabels = []
+    for conversation in (data['conversations']):
+        conversationText = list(map(lambda x: x["normText"], conversation["messages"]))
+        fullText = " ".join(conversationText)
+        TrainTexts.append(fullText)
+        TrainLabels.append(conversation["isCybergrooming"])
+    return TrainTexts,TrainLabels
+
+TrainTexts,TrainLabels = getData(trainJSON)
+estTexts,TestLabels = getData(testJSON)
+
+
+vectorizer = CountVectorizer(max_features=1000)
+
+label_encoder = LabelEncoder()
+
+train_x = vectorizer.fit_transform(TrainTexts)
+train_y = label_encoder.fit_transform(TrainLabels)
+
+test_x = vectorizer.transform(imdbTestTexts)
+test_y = label_encoder.transform(imdbTestLabels)
+
+
+#1.) Train Naive-Bayes with all features
+bayes = MultinomialNB()
+bayes.fit(train_x, train_y)
+pred = bayes.predict(test_x)
+print(accuracy_score(pred, test_y))
+print(confusion_matrix(test_y, pred))
+
+#2.) Perform frature selection in order to identify the most relevant features
+
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import mutual_info_classif
+from sklearn.feature_selection import SelectKBest
+
+selector = SelectKBest(score_func=mutual_info_classif, k=50)
+fit = selector.fit(train_x, train_y)
+
+vocab = vectorizer.vocabulary_
+inv_map = {v: k for k, v in vocab.items()}
+for value in selector.get_support(indices=True):
+    print(inv_map.get(value))
+    #print(selector.scores_[value])
+
+
+#3.) Reduce to selected features and train again
+trainX_transformed = selector.transform(train_x)
+testX_transformed = selector.transform(test_x)
+
+
+bayes = MultinomialNB()
+bayes.fit(trainX_transformed, train_y)
+pred = bayes.predict(testX_transformed)
+print(accuracy_score(pred, test_y))
+print(confusion_matrix(test_y, pred))
+
+
